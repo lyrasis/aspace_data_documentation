0ce38d2ca56d1572cbdf4b0d682ebd05a5a61ad4
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2026-04-10 20:38:18 -0400

    feat: Add PropertyConfigable mixin

---
+++ lib/aspace_data_tools/doc/property_configable.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module AspaceDataTools
+  module Doc
+    # Mixin module for determining field type and other information from
+    #   schema properties hash values
+    module PropertyConfigable
+      SIMPLE_TYPES = %w[string boolean date]
+
+      def read_only? = config.dig("readonly") == true
+
+      def config_includes_model?
+        config.to_s
+          .match?(/JSONModel\(:[^)]+\)/)
+      end
+
+      def subrecord?
+        config.to_s
+          .match?(/JSONModel\(:[^)]+\) (?:uri_or_)?object/)
+      end
+    end
+  end
+end
